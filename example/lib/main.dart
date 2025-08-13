0d8776c1f4a5ae67825bfd03d533fd5b0c7ec963
Brian <tranhieuglpk@gmail.com>
2025-08-13 14:58:02 +0700

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:bitmap_plus/bitmap.dart';
+import 'package:bitmap_plus/bitmap_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
